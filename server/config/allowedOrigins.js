a787527257d6488669961c53b3278c2228e5e642
Gysen Lionel <gysenlionel@gmail.com>
2022-07-31 18:42:32 +0200

    rename .env + remove swagger photos hotel

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = [process.env.CLIENT_URL, process.env.ADMIN_URL, process.env.SWAGGER_URL];
+const allowedOrigins = [process.env.CLIENT_URL, process.env.ADMIN_URL, process.env.API_URL];
 
 module.exports = allowedOrigins;
